5589b99fe0c4ed259b6f3b5b6b8f101e59b203cd
twomeetings <2326220@163.com>
2020-03-27 19:08:46 +0800

    chore: udpate

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -4,15 +4,15 @@ import ZHTW from './zh-TW.json'
 import EN from './en.json'
 
 const i18nMap = {
-	zh: ZHCN,
-	'zh-CN': ZHCN,
-	'zh-TW': ZHTW,
-	en: EN
+  zh: ZHCN,
+  'zh-CN': ZHCN,
+  'zh-TW': ZHTW,
+  en: EN
 }
 
-export default function(language) {
-	return new Polyglot({
-		phrases: i18nMap[language] || i18nMap.en,
-		locale: language
-	})
+export default function (language) {
+  return new Polyglot({
+    phrases: i18nMap[language] || i18nMap.en,
+    locale: language
+  })
 }
